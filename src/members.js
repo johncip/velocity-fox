a09d4e18b47276ac875b590d7e40f3ee2aba3a4e
John Cipriano <johnmikecip@gmail.com>
2018-03-13 03:55:26 -0400

    add members.js

---
+++ src/members.js
@@ -0,0 +1,91 @@
+export default { '59e6538f-0550-4e0f-8576-4de95c65a6a6':
+   { name: 'Danielle Buma',
+     mention_name: 'daniellebuma',
+     display_icon_url: undefined,
+     gravatar_hash: '1ded71b613c7bb3cd9897a487575bfde' },
+  '57682adf-f2e9-4665-922a-1de82893dde0':
+   { name: 'Saurabh Bipin Chandra',
+     mention_name: 'saurabh',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/57682adf-49ff-44d1-844b-02b1bf7a0148/593aed85-28ba-450b-9435-963e312ed2da/Saurabh_300x300.jpg',
+     gravatar_hash: '791a3978f8e12bd7c9d58a2fdcfbac41' },
+  '5824fadf-d04f-4f55-9e41-41155d2731c9':
+   { name: 'Gal ',
+     mention_name: 'gal',
+     display_icon_url: undefined,
+     gravatar_hash: 'c59498880010302e07220bcad496ad27' },
+  '59b71565-7634-400e-96de-94311e519839':
+   { name: 'Jerry Kennedy7',
+     mention_name: 'jerrykennedy7',
+     display_icon_url: undefined,
+     gravatar_hash: '8d91fe44accee4cf5bf28a7fe24848fa' },
+  '56fd532a-10e7-4dfd-b4d5-824210e9d73a':
+   { name: 'Sergey Karayev',
+     mention_name: 'sergeyk',
+     display_icon_url: undefined,
+     gravatar_hash: '1df6f02f6975bdf168f09f1c4dfde7ad' },
+  '56fcd1c3-600f-4c99-ba27-e716d54ea139':
+   { name: 'Arjun Singh',
+     mention_name: 'arjun',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/56fcd1c3-7609-482e-9c9a-92d24ea0c84f/58546cfe-8dd0-4a04-9569-c4f37432e85b/arjun.jpg',
+     gravatar_hash: '10b1bb94051043de9427062cdb5feb42' },
+  '57d05120-5f45-4e93-a0ac-dc60ebf39a62':
+   { name: 'Karen Ling',
+     mention_name: 'karenling',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/57d05120-5b5f-4a2f-8b32-2797f83ebbdc/59f35d79-9c3c-49ae-8854-21ae7c568407/DSCF2908.jpg_small.jpg',
+     gravatar_hash: '2a2f93b497383a6a7b361abaa1889836' },
+  '578d29ca-fda1-4a30-80c9-d3d502691a8c':
+   { name: 'Michael Ball',
+     mention_name: 'michaelball',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/578d29ca-602e-4d80-bd0a-796dac8771c6/58268864-a13b-49a6-8b80-a64323ff9491/Profile%20Square%20300.png',
+     gravatar_hash: 'ce804fea5cf01e721dab45ceb7b230b2' },
+  '56fd8a92-b6d2-4c32-84c2-d0446641ffce':
+   { name: 'Liz Carlson',
+     mention_name: 'lizcarlson',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/56fd8a92-824a-4166-9230-7efa5537ddca/58547434-ffdb-4015-a806-29d7e9577a3e/liz.jpg',
+     gravatar_hash: 'afc0caa78a085f365d86a5c7a43cab7c' },
+  '56fcd805-7c7e-4b64-a72b-6119ab4865f1':
+   { name: 'Ibrahim Awwal',
+     mention_name: 'ibrahim',
+     display_icon_url: undefined,
+     gravatar_hash: 'a57707d47d23eec0ccf6135bb40ece81' },
+  '581bc735-d646-413b-8201-6f49ac96b28f':
+   { name: 'Olga Korobova',
+     mention_name: 'olgakorobova',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/581bc735-b1a6-435f-b1c2-2fcb55f03066/58546b80-0f5b-4d4e-8c26-7a5d600c4795/olga.jpg',
+     gravatar_hash: '9b70ab01f8846040c593ace878832d2c' },
+  '59764c4a-244b-4c66-a637-83e622ef2588':
+   { name: 'Haoran Tang',
+     mention_name: 'haoran',
+     display_icon_url: undefined,
+     gravatar_hash: 'e039a72bb10b39f66ed5d6a4e481a802' },
+  '582f62b5-acb0-454b-bafd-04e77f0b460e':
+   { name: 'Dibyo Majumdar',
+     mention_name: 'dibyo',
+     display_icon_url: undefined,
+     gravatar_hash: 'd57be1ae5000fe75714e87e0f6b030a2' },
+  '59403dfd-cd83-484f-82b4-c94a8a77ff28':
+   { name: 'Aditya Baradwaj',
+     mention_name: 'adityabaradwaj',
+     display_icon_url: undefined,
+     gravatar_hash: '83315bb5dbff2ba28d0407592e75033c' },
+  '56fce13a-c391-4239-8334-e8afd2826988':
+   { name: 'John Cipriano',
+     mention_name: 'johncip',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/56fce13a-cfd8-4096-9079-f0f3808cd1bf/58546ee8-b4de-41f8-98de-b282fc6a3f28/IMG_5105.JPG',
+     gravatar_hash: '50f0ccd6496bd393c4a3799eb35efde3' },
+  '5797d6ce-1fa3-46ef-987f-cca00c085d22':
+   { name: 'Hezheng Yin',
+     mention_name: 'hezheng.yin',
+     display_icon_url: undefined,
+     gravatar_hash: '7e7d6c33027a6142ea2a2cbb1b6f501c' },
+  '582a3bf2-c163-4bce-b7d3-65dacba42533':
+   { name: 'Dawn garrison',
+     mention_name: 'dawngarrison',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/582a3bf2-0858-4e76-b7ba-7b3e3cb4db82/58548c42-4559-4a70-9f75-aceda5ec36a7/dawn%20pic.png',
+     gravatar_hash: 'e2f538ecf568ecbbbebebf23dff58fd9' },
+  '56fda91c-cc40-4927-a873-05a729d8488d':
+   { name: 'Kevin Gutowski',
+     mention_name: 'kev',
+     display_icon_url: 'https://api.clubhouse.io/api/attachments/user-icons/clubhouse-assets/56fda91c-cad2-4312-9ee3-843bb946f0a4/5858249e-7376-4451-803f-30563400fc73/profile.jpg',
+     gravatar_hash: '1aac4cdd78aedc29d812ce011f8aff47' }
+}
