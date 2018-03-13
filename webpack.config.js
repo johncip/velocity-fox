ea5ea10287fc5c5a5bb301b979ce7af7d01adb0f
John Cipriano <johnmikecip@gmail.com>
2018-03-13 03:55:17 -0400

    change page title

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Velocity Fox',
       favicon: path.resolve(__dirname, 'assets', 'favicon.png'),
     }),
     new CleanWebpackPlugin(['dist']),
