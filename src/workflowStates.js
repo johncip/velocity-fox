a2c1daf5d1200d21246706cbcdbbdf1fbd31d181
John Cipriano <johnmikecip@gmail.com>
2018-03-13 03:55:50 -0400

    add workflowStates.js

---
+++ src/workflowStates.js
@@ -0,0 +1,9 @@
+export default {
+  500000014: 'Unscheduled',
+  500000011: 'Ready for Development',
+  500007040: 'Next Up',
+  500000015: 'In Development',
+  500000010: 'Ready for Review',
+  500000012: 'Completed'
+}
+
