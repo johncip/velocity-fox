a4bcf9869e0ed7234033b8a28dec51a911065e3d
John Cipriano <johnmikecip@gmail.com>
2018-03-12 20:53:46 -0400

    switch to react

--- src/index.js
+++ src/index.js
@@ -1,34 +1,69 @@
-import $ from 'jquery';
-import Clubhouse from 'clubhouse-lib';
 import '../assets/styles/style.scss';
+
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Clubhouse from 'clubhouse-lib';
 import Icon from '../assets/pretty.png';
 import secrets from 'Config/secrets.js';
 
-function foxImage() {
-  return $('<img>')
-    .attr('src', Icon)
-    .width(200);
+
+const Heading = props =>
+  <h1 className="siteHeading">
+    <img src={Icon} height={50} />
+    &nbsp;
+    Velocity Fox
+  </h1>
+
+// TODO: exclude .disabled
+// TODO: exclude profile.deactivated ?
+const MemberList = ({members}) =>
+  <ul className="memberList">{
+    members.map((m) =>
+      <li key={m.id}><Member {...m} /></li>
+    )
+  }</ul>
+
+const Member = props =>
+  <ul>
+    <li>{`entity type: ${props.entity_type}`}</li>
+    <li>{`id: ${props.id}`}</li>
+    <li>{`role: ${props.role}`}</li>
+    <li>{`disabled: ${props.disabled}`}</li>
+    <li>profile: <Profile {...props.profile} /></li>
+  </ul>
+
+const Profile = props =>
+  <ul>
+    <strong><li>{`${props.name}`}</li></strong>
+    <strong><li>{`@${props.mention_name}`}</li></strong>
+    <li>{`email_address: ${props.email_address}`}</li>
+    <li>{`entity_type: ${props.entity_type}`}</li>
+    {/* <li> */}
+    {/*   <img src={`https://www.gravatar.com/avatar/${props.gravatar_hash}.jpg`}/> */}
+    {/* </li> */}
+    <li>{`id: ${props.id}`}</li>
+  </ul>
+
+class Root extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <Heading/>
+        <MemberList members={this.props.members}/>
+      </div>
+    );
+  }
 }
 
-function heading() {
-  return $('<h1>')
-    .text('Velocity Fox');
-}
+const clubhouse = Clubhouse.create(secrets.clubhouse);
 
-function hello() {
-  return $('<div>')
-    .addClass('hello')
-    .html('hello2');
-}
+const $target = $('<div>').addClass('.js-target')
+$(document.body).append($target)
 
-function members() {
-  const clubhouse = Clubhouse.create(secrets.clubhouse);
-  clubhouse.listMembers().then(console.log);
-}
+clubhouse.listMembers().then((x) =>
+  ReactDOM.render(<Root members={x}/>, $target[0])
+);
 
-$(document.body)
-  .append(heading())
-  .append(foxImage())
-  .append(hello())
-
-members();
+// Dibyo member ID: 582f62b5-acb0-454b-bafd-04e77f0b460e
+// Dibyo profile ID: 582f62b5-5836-48dd-944a-d8ef1681ae1a
