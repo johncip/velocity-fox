0f26eaee8a6de070bc9fed11103d176e4a22dfb1
John Cipriano <johnmikecip@gmail.com>
2018-03-13 16:14:53 -0400

    shorten to "ready for dev"

--- src/workflowStates.js
+++ src/workflowStates.js
@@ -1,6 +1,6 @@
 export default {
   500000014: 'Unscheduled',
-  500000011: 'Ready for Development',
+  500000011: 'Ready for Dev',
   500007040: 'Next Up',
   500000015: 'In Development',
   500000010: 'Ready for Review',
