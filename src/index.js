ba19f3f46cd0bf8500fb0ecb29f8ab513bb2adcf
John Cipriano <johnmikecip@gmail.com>
2018-03-14 23:54:27 -0400

    reorganize jsx

--- src/index.js
+++ src/index.js
@@ -7,6 +7,99 @@ import classNames from 'classnames';
 import secrets from 'Config/secrets.js';
 import workflowStates from './workflowStates.js';
 
+// ---------------------------------------------------------------------------------------
+// index
+// ---------------------------------------------------------------------------------------
+
+import $ from 'jquery';
+
+$(document).ready(() => {
+  const $target = $('<div>').addClass('.js-target')
+  $(document.body).append($target)
+  ReactDOM.render(<AppRoot />, $target[0]);
+});
+
+
+// ---------------------------------------------------------------------------------------
+// AppRoot
+// ---------------------------------------------------------------------------------------
+
+import _ from 'lodash';
+import Clubhouse from 'clubhouse-lib';
+
+const PROJECT_IDS = {
+  web_app: 5,
+}
+
+const NULL_OWNER_ID = 'no-owner';
+
+// Given a list of stories & called from Array.reduce, creates a map: {owner_id => [stories]}
+const indexStoriesByOwner = (result, story) => {
+  if (!story.owner_ids.length) {
+    story.owner_ids.push(NULL_OWNER_ID);
+  }
+
+  story.owner_ids.forEach((id) => {
+    if (!result[id]) {
+      result[id] = [];
+    }
+    result[id].push(story);
+  });
+
+  return result;
+};
+
+class AppRoot extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {groupedStories: null};
+  }
+
+  componentDidMount() {
+    const clubhouseClient = Clubhouse.create(secrets.clubhouse);
+    clubhouseClient.listStories(PROJECT_IDS.web_app).then((response) => {
+      const groupedStories = response.reduce(indexStoriesByOwner, {});
+      this.setState({groupedStories});
+    });
+  }
+
+  renderMemberList() {
+    return (
+      <MemberList
+        members={members}
+        groupedStories={this.state.groupedStories}
+        showUnscheduled={false}
+        showCompleted={false}
+        showArchived={false}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="content">
+          {this.state.groupedStories ? this.renderMemberList() : <Loading />}
+        </div>
+      </div>
+    );
+  }
+}
+
+const Loading = props =>
+  <div className="loading">
+    <div className="lds-dual-ring" />
+  </div>
+
+import foxIcon from '../assets/pretty.png';
+
+const Header = props =>
+  <header className="header">
+    <img src={foxIcon} height={50} />
+  </header>
+
+
 // -------------------------------------------------------------------------------------------
 // MemberList
 // -------------------------------------------------------------------------------------------
@@ -45,20 +138,8 @@ class MemberList extends React.PureComponent {
 }
 
 // -------------------------------------------------------------------------------------------
-// Misc Components
+// Story Components
 // -------------------------------------------------------------------------------------------
-
-import foxIcon from '../assets/pretty.png';
-
-const Loading = props =>
-  <div className="loading">
-    <div className="lds-dual-ring" />
-  </div>
-
-const Header = props =>
-  <header className="header">
-    <img src={foxIcon} height={50} />
-  </header>
 
 // TODO: hide stories here
 class StoryList extends React.PureComponent {
@@ -136,81 +217,3 @@ class StoryType extends React.PureComponent {
   }
 }
 
-// ---------------------------------------------------------------------------------------
-// AppRoot
-// ---------------------------------------------------------------------------------------
-
-import _ from 'lodash';
-import Clubhouse from 'clubhouse-lib';
-
-const PROJECT_IDS = {
-  web_app: 5,
-}
-
-const NULL_OWNER_ID = 'no-owner';
-
-// Given a list of stories & called from Array.reduce, creates a map: {owner_id => [stories]}
-const indexStoriesByOwner = (result, story) => {
-  if (!story.owner_ids.length) {
-    story.owner_ids.push(NULL_OWNER_ID);
-  }
-
-  story.owner_ids.forEach((id) => {
-    if (!result[id]) {
-      result[id] = [];
-    }
-    result[id].push(story);
-  });
-
-  return result;
-};
-
-class AppRoot extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {groupedStories: null};
-  }
-
-  componentDidMount() {
-    const clubhouseClient = Clubhouse.create(secrets.clubhouse);
-    clubhouseClient.listStories(PROJECT_IDS.web_app).then((response) => {
-      const groupedStories = response.reduce(indexStoriesByOwner, {});
-      this.setState({groupedStories});
-    });
-  }
-
-  renderMemberList() {
-    return (
-      <MemberList
-        members={members}
-        groupedStories={this.state.groupedStories}
-        showUnscheduled={false}
-        showCompleted={false}
-        showArchived={false}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="content">
-          {this.state.groupedStories ? this.renderMemberList() : <Loading />}
-        </div>
-      </div>
-    );
-  }
-}
-
-// ---------------------------------------------------------------------------------------
-// index
-// ---------------------------------------------------------------------------------------
-
-import $ from 'jquery';
-
-$(document).ready(() => {
-  const $target = $('<div>').addClass('.js-target')
-  $(document.body).append($target)
-  ReactDOM.render(<AppRoot />, $target[0]);
-});
